f91171b07e92a5e23e4a1dc6fc348781a3a2283e
Ben King <ben.king@easeltv.com>
2020-08-26 20:06:10 +0100

    Use bounding client rect

--- src/measureLayout.js
+++ src/measureLayout.js
@@ -1,23 +1,11 @@
-const ELEMENT_NODE = 1;
-
 const getRect = (node) => {
-  let {offsetParent} = node;
-  const height = node.offsetHeight;
-  const width = node.offsetWidth;
-  let left = node.offsetLeft;
-  let top = node.offsetTop;
-
-  while (offsetParent && offsetParent.nodeType === ELEMENT_NODE) {
-    left += offsetParent.offsetLeft - offsetParent.scrollLeft;
-    top += offsetParent.offsetTop - offsetParent.scrollTop;
-    ({offsetParent} = offsetParent);
-  }
+  const rect = node.getBoundingClientRect();
 
   return {
-    height,
-    left,
-    top,
-    width
+    height: Math.ceil(rect.height),
+    left: Math.ceil(rect.left),
+    top: Math.ceil(rect.top),
+    width: Math.ceil(rect.width)
   };
 };
 
